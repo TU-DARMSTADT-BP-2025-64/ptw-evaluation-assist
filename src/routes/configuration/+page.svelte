fab081a92845e3069c14bb8473d1984927ee4762
Ayush Bansal <bansl.ayush@gmail.com>
2024-12-04 21:45:03 +0100

    Added the button to the form

--- src/routes/configuration/+page.svelte
+++ src/routes/configuration/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { HeaderService } from '../HeaderService.svelte';
-    
+    import Ripple from '@smui/ripple';
+
     HeaderService.Instance.setTitle('Konfiguration');
 </script>
 
@@ -10,9 +11,40 @@
 </svelte:head>
 
 <section>
-    Konfiguration
+    <a href="/configuration/form">
+    <div
+      class="card-btn mdc-elevation--z2"
+      use:Ripple={{ surface: true }}
+      onkeydown={() => {}}
+      role="button"
+      tabindex="0"
+    >
+        <div class="icon">
+            <i class="material-icons">add</i>
+        </div>
+    </div>
+    </a>
 </section>
 
 <style>
-    
+    .card-btn {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        padding: 12px;
+        border-radius: 8px;
+        background: white;
+        cursor: pointer;
+        margin-right: 1500px;
+        position: relative; /* Necessary for absolute positioning of label */
+        width: 48px; /* Adjust width to fit the button */
+        height: 48px; /* Adjust height to fit the button */
+    }
+
+    .card-btn .icon i {
+        font-size: 60px;
+        height: 60px;
+        width: 60px;
+        color: #b81018;
+    }
 </style>
---
+++ src/routes/configuration/form/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { HeaderService } from './../../HeaderService.svelte';
+	import Textfield from '@smui/textfield';
+	HeaderService.Instance.setTitle('Konfiguration');
+	let valueA = $state('');
+//liste von den baugruppen erstellen und dann mit dem plus oder minus ein
+	// element entfernen oder hinzufügen
+</script>
+
+<body>
+<h1>Produkt hinzufügen</h1>
+</body>
+
+<section>
+
+	<div class="pname">
+		<div>
+			<Textfield variant="filled" bind:value={valueA} label="Produkt Name">
+			</Textfield>
+		</div>
+	</div>
+
+	<div class="Baugruppe">
+		<div>
+			<Textfield variant="filled" bind:value={valueA} label="Baugruppe">
+			</Textfield>
+		</div>
+	</div>
+
+
+</section>
+
+<style>
+    h1 {
+        position: fixed;
+        top: 80px; /* Abstand vom oberen Rand */
+        left: 30px; /* Abstand vom linken Rand */
+    }
+		.pname{
+			position: fixed;
+			top: 150px;
+			left: 60px;
+		}
+		.Baugruppe{
+			position: fixed;
+			top: 230px;
+			left: 60px;
+				cursor: crosshair;
+		}
+</style>
