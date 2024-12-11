a70597a673ab68439c0698fa8616d6d0b0cedfab
miriambw <miriamb2022@gmx.de>
2024-12-11 18:18:45 +0100

    suchleiste auswahl der unterbaugruppen

--- src/routes/assistant/+page.svelte
+++ src/routes/assistant/+page.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
 	import { HeaderService } from '../HeaderService.svelte';
 	import Textfield from '@smui/textfield';
-
+	import {onMount}from 'svelte';
 	let value = $state('');
+	let products =[];
+	let subgroups: string | any[]=[];
+	let selectedProduct='';
+	let selectedSubgroup='';
     
     HeaderService.Instance.setTitle('Assistent');
+//Produkte laden
+	onMount(async()=> {products= await fetchProducts();
+
+});
+// Produkte von der API abrufen
+async function fetchProducts(){
+
+const response = await fetch ('/api/products');
+return await response.json();
+}
+//funtion Unterbaugruppen für ein Produkt abrufen
+async function fetchSubgroups(product:string) {
+	const response = await fetch(`/api/subgroups?product=${encodeURIComponent(product)}`); 
+		subgroups = await response.json(); 
+	
+}
+
+
 </script>
 
 <svelte:head>
@@ -24,6 +46,26 @@
     
 	
 </section>
+<div>
+	<Textfield
+	style="width: 100%;"
+			bind:value={selectedProduct}
+			label="Unterbaugruppe suchen"
+			class="solo-input"
+			on:change={() => fetchSubgroups(selectedProduct)} />
+</div>
+
+{#if subgroups.length > 0}
+		<div>
+			<label for="subgroupSelect">Unterbaugruppe auswählen:</label>
+			<select id="subgroupSelect" bind:value={selectedSubgroup}>
+				{#each subgroups as subgroup}
+					<option value={subgroup}>{subgroup}</option>
+				{/each}
+			</select>
+		</div>
+	{/if}
+
 
 <style>
 </style>
