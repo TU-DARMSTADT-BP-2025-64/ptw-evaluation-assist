c9eff3b0a984134b28a165960ede852bf4d0a475
Jacob Deuchert <jacobdeuchert@gmail.com>
2024-12-20 00:31:50 +0100

    feat: add style to config and assistance products page

--- src/lib/components/ProductTable/ProductTable.svelte
+++ src/lib/components/ProductTable/ProductTable.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
 	import DataTable, { Head, Body, Row, Cell, Pagination } from '@smui/data-table';
+	import TextField from '@smui/textfield';
 	import Select, { Option } from '@smui/select';
 	import IconButton from '@smui/icon-button';
 	import { Label } from '@smui/common';
 	import type { ProductViewModel } from '$lib/models/product.model';
+  	import { Icon } from '@smui/common';
 
 	let props: { products: ProductViewModel[] } = $props();
 
 	// Reaktive Variable für den Suchbegriff
-	let searchQuery = $state("");
+	let searchQuery = $state('');
 
 	// Filtere die Daten basierend auf dem Suchbegriff
 	const filteredData = $derived.by(() => {
-		return props.products.filter(product =>
-			product.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-			product.id === Number(searchQuery)
+		return props.products.filter(
+			(product) =>
+				product.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+				product.id === Number(searchQuery)
 		);
 	});
 
@@ -35,91 +38,98 @@
 	});
 </script>
 
-<style>
-    .search-bar {
-        margin-bottom: 1rem;
-    }
-</style>
-
-<!-- Suchleiste -->
-<div class="search-bar">
-	<input
-		type="text"
-		placeholder="Search by ID or name..."
-		bind:value={searchQuery}
-		style="width: 100%; padding: 0.5rem; font-size: 1rem;"
-	/>
+<div class="product-table">
+	<!-- Suchleiste -->
+	<div class="search-bar mdc-elevation--z1">
+		<TextField style="width: 100%" variant="filled" bind:value={searchQuery} label="Suche">
+			{#snippet leadingIcon()}
+			  <Icon style="margin-left: 8px; margin-right: 4px" class="material-icons">search</Icon>
+			{/snippet}
+		  </TextField>
+	</div>
+	
+	<div class="mdc-elevation--z1" style="width: 100%; overflow: auto;">
+		<!-- Tabelle -->
+		<DataTable table$aria-label="Product List" style="width: 100%;">
+			<Head>
+				<Row>
+					<Cell>ID</Cell>
+					<Cell>Name</Cell>
+				</Row>
+			</Head>
+			<Body>
+				{#each slice as { id, name }}
+					<Row>
+						<Cell>{id}</Cell>
+						<Cell>{name}</Cell>
+					</Row>
+				{/each}
+			</Body>
+		
+			<!-- Pagination -->
+			{#snippet paginate()}
+				<Pagination>
+					<Label>Produkte pro Seite</Label>
+					<Select variant="outlined" bind:value={perPage} noLabel>
+						<Option value={10}>10</Option>
+						<Option value={25}>25</Option>
+						<Option value={100}>100</Option>
+					</Select>
+					{#snippet total()}
+						{start + 1}-{end} of {filteredData.length}
+					{/snippet}
+		
+					<!-- Buttons for Pagination -->
+					<IconButton
+						class="material-icons"
+						action="first-page"
+						title="First page"
+						onclick={() => (currentPage = 0)}
+						disabled={currentPage === 0}>first_page</IconButton
+					>
+		
+					<IconButton
+						class="material-icons"
+						action="prev-page"
+						title="Prev page"
+						onclick={() => currentPage--}
+						disabled={currentPage === 0}>chevron_left</IconButton
+					>
+		
+					<IconButton
+						class="material-icons"
+						action="next-page"
+						title="Next page"
+						onclick={() => currentPage++}
+						disabled={currentPage === lastPage}>chevron_right</IconButton
+					>
+		
+					<IconButton
+						class="material-icons"
+						action="last-page"
+						title="Last page"
+						onclick={() => (currentPage = lastPage)}
+						disabled={currentPage === lastPage}>last_page</IconButton
+					>
+				</Pagination>
+			{/snippet}
+		</DataTable>
+	</div>
 </div>
 
-<!-- Tabelle -->
-<DataTable table$aria-label="Product List" style="max-width: 100%;">
-	<Head>
-		<Row>
-			<Cell>ID</Cell>
-			<Cell>Name</Cell>
-		</Row>
-	</Head>
-	<Body>
-	{#each slice as { id, name }}
-		<Row>
-			<Cell>{id}</Cell>
-			<Cell>{name}</Cell>
-		</Row>
-	{/each}
-	</Body>
+<style>
 
-	<!-- Pagination -->
-	{#snippet paginate()}
-		<Pagination>
-			<Label>Rows Per Page</Label>
-			<Select variant="outlined" bind:value={perPage} noLabel>
-				<Option value={10}>10</Option>
-				<Option value={25}>25</Option>
-				<Option value={100}>100</Option>
-			</Select>
-			{#snippet total()}
-				{start + 1}-{end} of {filteredData.length}
-			{/snippet}
+	.product-table {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
-
-
-			<!-- Buttons for Pagination -->
-			<IconButton
-				class="material-icons"
-				action="first-page"
-				title="First page"
-				onclick={() => currentPage = 0}
-				disabled={currentPage === 0}
-			>first_page</IconButton>
-
-			<IconButton
-				class="material-icons"
-				action="prev-page"
-				title="Prev page"
-				onclick={() => currentPage--}
-				disabled={currentPage === 0}
-			>chevron_left</IconButton>
-
-			<IconButton
-				class="material-icons"
-				action="next-page"
-				title="Next page"
-				onclick={() => currentPage++}
-				disabled={currentPage === lastPage}
-			>chevron_right</IconButton>
-
-			<IconButton
-				class="material-icons"
-				action="last-page"
-				title="Last page"
-				onclick={() => currentPage = lastPage}
-				disabled={currentPage === lastPage}
-			>last_page</IconButton>
-		</Pagination>
-		{/snippet}
-</DataTable>
-
-
-
-
-
+	}
+	.search-bar {
+		margin-top: 2rem;
+		margin-bottom: 2rem;
+		width: 100%;
+		max-width: min(100%, 400px);
+	}
+</style>
--- src/routes/configuration/+page.svelte
+++ src/routes/configuration/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { HeaderService } from '../HeaderService.svelte';
+    import { Icon } from '@smui/common';
+	import Button from '@smui/button';
+	import { goto } from '$app/navigation';
     import ProductTable from '$lib/components/ProductTable/ProductTable.svelte';
     import type { ProductViewModel } from '$lib/models/product.model';
     
@@ -26,12 +29,53 @@
     <meta name="description" content="PTW evaluation assist" />
 </svelte:head>
 
-<section>
-    Konfiguration
-    <button onclick="{() => addProduct()}">Add product</button>
-    <ProductTable products = {props.data.products} />
+<section class="page">
+    <div class="back-button">
+		<Button  onclick={() => goto('/')}>
+			<Icon class="material-icons">arrow_back</Icon>
+			<span>Zurück</span>
+		</Button>
+	</div>
+
+	<div class="product-table">
+		<ProductTable products = {props.data.products} />
+	</div>
+
+    <div class="add-product-button">
+		<Button variant="raised" color="secondary"  onclick={() => goto('/configuration/add')}>
+			<Icon class="material-icons">add</Icon>
+			<span>Produkt hinzufügen</span>
+		</Button>
+	</div>
 </section>
 
 <style>
+
+.page {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.back-button {
+		position: absolute;
+		top: 20px;
+		left: 20px;
+		z-index: 100;
+	}
+
+	.product-table {
+		width: 100%;
+		max-width: min(100%, 800px);
+	}
+
+    .add-product-button {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+    }
     
 </style>
