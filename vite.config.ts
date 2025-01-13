137034411ca5bc0544688cf672ee581b681c5716
Jacob Deuchert <jacobdeuchert@gmail.com>
2025-01-13 15:24:30 +0100

    update vite and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vitest/config';
-import {svelteTesting} from '@testing-library/svelte/vite'
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), svelteTesting()],
-	
+
 	test: {
 		environment: 'jsdom',
+		globals: true,
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		server: {
 			deps: {
