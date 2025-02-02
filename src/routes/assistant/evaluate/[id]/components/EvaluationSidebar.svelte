54828a04922f8371dca47c9dfef7da3741eb05e5
Jacob Deuchert <jacobdeuchert@gmail.com>
2025-02-02 13:24:27 +0100

    feat: sidebar treeview

---
+++ src/routes/assistant/evaluate/[id]/components/EvaluationSidebar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { EvaluatedAssemblyComponentTreeViewModel, EvaluatedProductTreeViewModel } from '$lib/components/ComponentSelectDialog/EvaluatedTreeView.svelte';
+	import SidebarAssemblyGroup from './SidebarAssemblyGroup.svelte';
+
+	let {
+		productTreeView,
+		selectedComponent = $bindable()
+	}: {
+		productTreeView: EvaluatedProductTreeViewModel;
+		selectedComponent: EvaluatedAssemblyComponentTreeViewModel | null;
+		
+	} = $props();
+</script>
+
+<div class="sidebar">
+	<div class="title">Komponenten</div>
+	<div class="components-container">
+		{#each productTreeView.assemblyGroups as group, i}
+			<SidebarAssemblyGroup
+				level={0}
+				assemblyGroup={group}
+				bind:selectedComponent={selectedComponent} />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.sidebar {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.title {
+		font-size: 1.2rem;
+		font-weight: bold;
+	}
+
+
+	.components-container{
+      height: 100%;
+      overflow-y: auto;
+	}
+
+	
+</style>
